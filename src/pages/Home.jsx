1c6ac8d44f1baf429641aa02f6bfe4ed42899a94
abdou251 <abdallah05boudiaf@gmail.com>
2023-03-01 15:25:15 +0100

    style the product list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,8 @@ const Home = () => {
 
   return (
     <div className='w-full'>
-      <aside className=' w-1/3'>hi</aside>
-      <div className='w-3/4'>
-        <ul className='flex justify-between items-center flex-wrap m-12   '>
+      <div className=''>
+        <ul className='flex justify-evenly flex-wrap gap-3'>
           {products.map((item) => {
             return <Products key={item.id} {...item} />
           })}
@@ -23,11 +22,13 @@ const Home = () => {
 const Products = (props) => {
   const { image, id, title, price } = props
   return (
-    <div className='flex-col items-center justify-center ' key={id}>
-      <img className=' w-28' src={image} alt='' />
-      <div className='flex gap-3'>
-        <p>{title}</p>
-        <p>{price}</p>
+    <div className=' w-64 mb-2 p-3 border-2 rounded-lg' key={id}>
+      <div className='flex-col'>
+        <img className='flex w-100 h-44' src={image} alt='' />
+        <div className='flex items-center gap-3'>
+          <h3 className='my-4'>{title}</h3>
+          <p className='font-bold'>{price}</p>
+        </div>
       </div>
       <button className='bg-slate-300 p-2'>Add to cart</button>
     </div>
